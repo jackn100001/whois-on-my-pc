2b2a680151dd48b93586dde03ae768df1beb6863
Jack Nicol <jack-nicol1@hotmail.co.uk>
2020-03-27 00:34:14 +0000

    display connection count for countries

--- whois.py
+++ whois.py
@@ -11,8 +11,21 @@ import json
 from ipwhois import IPWhois
 
 connections = psutil.net_connections()
+countries = {}
+
 for conn in connections:
 	if (conn.raddr):
-		print("Local: {}, Remote: {}".format(conn.laddr[0], conn.raddr[0]))
-		obj = json.dumps(IPWhois(str(conn.raddr[0])).lookup_whois(), indent=4)
-		print(obj)
+		raddr = conn.raddr[0]
+		if (not raddr.startswith('192') and not raddr.startswith(":") and not raddr.startswith("127")):
+			print("Local: {}, Remote: {}".format(conn.laddr[0], conn.raddr[0]))
+			netstat = IPWhois(str(conn.raddr[0])).lookup_whois()
+			jsonOut = json.dumps(netstat, indent=4)
+			# print(json.dumps(netstat["nets"][0], indent=4))
+			country = netstat["nets"][0]["country"]
+			if (country in countries):
+				countries[country] += 1
+			else:
+				countries[country] = 1
+
+for country in countries:
+	print(country, countries.get(country))
